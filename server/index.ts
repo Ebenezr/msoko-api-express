fb32fb44b382ec55565c21a83a4c85b24e526514
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-10 19:12:37 +0300

    [ADD] setup routes

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 
+import userRouter from "./controllers/user.controller";
+import productRouter from "./controllers/product.controller";
+import categoryRouter from "./controllers/category.controller";
+import inventoryRouter from "./controllers/inventory.controller";
+import discountRouter from "./controllers/discount.controller";
+import orderRouter from "./controllers/order.controller";
+import orderListRouter from "./controllers/order_list.controller";
+import reviewsRouter from "./controllers/reviews.controller";
+import addressRouter from "./controllers/address.controller";
+
 const responseTime = require("response-time");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -25,6 +35,17 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req: Request, res: Response) => {
   res.json({ status: "API is running on /api" });
 });
+
+// routes
+app.use("/api", userRouter);
+app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+app.use("/api", inventoryRouter);
+app.use("/api", discountRouter);
+app.use("/api", orderRouter);
+app.use("/api", orderListRouter);
+app.use("/api", reviewsRouter);
+app.use("/api", addressRouter);
 
 app.listen(PORT, () =>
   console.log(`REST API server ready at: http://localhost:${PORT}`)

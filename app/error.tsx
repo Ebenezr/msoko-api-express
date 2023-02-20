b8b5b2a3d39908ea2d2462c44e65edb73e85e3aa
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-20 14:01:16 +0300

    [REF] customise error page

--- app/error.tsx
+++ app/error.tsx
@@ -15,9 +15,15 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>No connection</h2>
+    <div className="w-full h-full flex flex-col space-y-4 text-center">
+      <h2 className="text-gray-700">No connection</h2>
+      {/* icon wifi */}
+      <p className="text-slate-500 text-xs">
+        An internet error occurred, please try again{" "}
+      </p>
+
       <button
+        className="w-full bg-slate-400 text-gray-700 font-semibold py-2 px-4"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()

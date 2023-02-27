1057874a7cb1609faacb32e8ebafec2b570abfc2
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-27 09:07:05 +0300

    [ADD] add redis middleware

---
+++ src/middleware/redis.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response, Router } from "express";
+// import redisClient from "../utils/connectRedis";
+const redis = require("redis");
+export const redisClient = redis.createClient({
+  url: "redis://localhost:6379",
+});
+(async () => {
+  redisClient.on("error", (error: any) => console.error(`Ups : ${error}`));
+  await redisClient.connect();
+})();
+
+redisClient.on("connect", () => {
+  console.log("Redis client connected");
+});
+
+redisClient.on("error", (err: any) => {
+  console.log("Redis error: ", err);
+});
+
+const cacheMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const { query } = req;
+  const cacheKey = JSON.stringify(query);
+
+  redisClient.get(cacheKey, (err: any, data: any) => {
+    if (err) throw err;
+
+    if (data != null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  });
+};
+
+export default cacheMiddleware;

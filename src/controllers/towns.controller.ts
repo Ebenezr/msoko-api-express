2dd756d5f446ad5a11b0cbda41b9577844abb7c1
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-28 10:12:27 +0300

    [FIX]

--- src/controllers/towns.controller.ts
+++ src/controllers/towns.controller.ts
@@ -4,7 +4,7 @@ const redis = require("redis");
 const cache = require("express-redis-cache")();
 
 // create client with URL
-const client = redis.createClient("redis://localhost:6379");
+const client = redis.createClient("redis://redis:6379");
 
 const prisma = new PrismaClient();
 const router = Router();

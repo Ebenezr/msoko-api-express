3ba51ce9e973992208fc3a4ba8c2e1afde3b8e18
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-08 16:39:06 +0300

    [REF] add test to check all global variables

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -7,7 +7,9 @@ const redis = require("redis");
 const cache = require("express-redis-cache")();
 
 // create client with URL
-const client = redis.createClient("redis:redis:6379");
+const client = redis.createClient(
+  "redis://default:6WoKnd5yQ36bq4kDc596@containers-us-west-201.railway.app:7546"
+);
 
 const prisma = new PrismaClient();
 const router = Router();

6f9c486c18553cf0a209fb0d9bce7cc2df58aab1
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-08 16:29:31 +0300

    [REF] add test to check all global variables

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,12 +3,10 @@ import { cleanEnv, port, str } from "envalid";
 function validateEnv() {
   cleanEnv(process.env, {
     DATABASE_URL: str(),
-
-    POSTGRES_HOST: str(),
-    POSTGRES_PORT: port(),
-    POSTGRES_USER: str(),
-    POSTGRES_PASSWORD: str(),
-    POSTGRES_DB: str(),
+    CLOUDINARY_CLOUD_NAME: str(),
+    CLOUDINARY_API_KEY: str(),
+    CLOUDINARY_API_SECRET: str(),
+    JWT_SECRET: str(),
   });
 }
 

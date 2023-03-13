37d6a586c2449aa1b4b30855e44a550b2782a5dd
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-13 11:20:25 +0300

    [REF] fix auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,36 +1,31 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { PrismaClient } from "@prisma/client";
 import { AnyZodObject } from "zod";
-import { createUserSchema, loginUserSchema } from "../schemas/user.schema";
-const bcrypt = require("bcryptjs");
+import { default as bcrypt } from "bcryptjs";
 const jwt = require("jsonwebtoken");
 const prisma = new PrismaClient();
 const router = Router();
 
-const validate =
-  (schema: AnyZodObject) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
-      return next();
-    } catch (error) {
-      return res.status(400).json(error);
-    }
-  };
+// const validate =
+//   (schema: AnyZodObject) =>
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       await schema.parseAsync({
+//         body: req.body,
+//         query: req.query,
+//         params: req.params,
+//       });
+//       return next();
+//     } catch (error) {
+//       return res.status(400).json(error);
+//     }
+//   };
 
 router.post(
   "/login",
-
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { email, password } = req.body;
-
-      // query the appropriate table based on the user's role
-
       const user = await prisma.user.findUnique({ where: { email } });
       if (!user) {
         // return an error if
@@ -38,8 +33,9 @@ router.post(
           .status(400)
           .json({ success: false, error: "Invalid email or password" });
       }
+      console.log(user);
 
-      const passwordMatch = await bcrypt.compare(password, user.password);
+      const passwordMatch = await bcrypt.compare(password, user.password ?? "");
       if (!passwordMatch) {
         return res
           .status(400)
@@ -53,13 +49,13 @@ router.post(
           email: user.email,
         },
         process.env.JWT_SECRET,
-        { expiresIn: "7d" }
+        { expiresIn: "1d" }
       );
 
       res.json({
         success: true,
         token,
-        name: user.name,
+        name: user.email,
       });
     } catch (error) {
       next(error);
